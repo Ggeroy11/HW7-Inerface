9ef5402e582d6c7894ac94b96f7026b4bbad57a1
УважановЕ <@209-STUD-11>
2017-09-10 11:40:06 +0600

    Еще не достроенный дом

---
+++ ConsoleApplication1/ConsoleApplication1/House.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class House
+    {
+        List<IPart> part = new List<IPart>();
+        public House()
+        {
+            part.Add(new Basement());
+            part.Add(new Wall());
+            part.Add(new Wall());
+            part.Add(new Wall());
+            part.Add(new Wall());
+            part.Add(new Door());
+            part.Add(new Window());
+            part.Add(new Window());
+            part.Add(new Window());
+            part.Add(new Window());
+            part.Add(new Roof());
+        }
+       
+
+
+    }
+}
